2e43ab072b1f87584f04240b45d43b05445f0740
Andrew Obuchowicz <ako@sincere.com>
2025-04-10 17:13:09 -0400

    Add sync.Once to Configure() to prevent multiple initializations
    Signed-off-by: Andrew Obuchowicz <ako@sincere.com>

--- config.go
+++ config.go
@@ -2,6 +2,7 @@ package workers
 
 import (
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/garyburd/redigo/redis"
@@ -15,67 +16,72 @@ type config struct {
 	Fetch        func(queue string) Fetcher
 }
 
-var Config *config
+var (
+	Config     *config
+	configOnce sync.Once
+)
 
 func Configure(options map[string]string) {
-	var poolSize int
-	var namespace string
-	var pollInterval int
+	configOnce.Do(func() {
+		var poolSize int
+		var namespace string
+		var pollInterval int
 
-	if options["server"] == "" {
-		panic("Configure requires a 'server' option, which identifies a Redis instance")
-	}
-	if options["process"] == "" {
-		panic("Configure requires a 'process' option, which uniquely identifies this instance")
-	}
-	if options["pool"] == "" {
-		options["pool"] = "1"
-	}
-	if options["namespace"] != "" {
-		namespace = options["namespace"] + ":"
-	}
-	if seconds, err := strconv.Atoi(options["poll_interval"]); err == nil {
-		pollInterval = seconds
-	} else {
-		pollInterval = 15
-	}
+		if options["server"] == "" {
+			panic("Configure requires a 'server' option, which identifies a Redis instance")
+		}
+		if options["process"] == "" {
+			panic("Configure requires a 'process' option, which uniquely identifies this instance")
+		}
+		if options["pool"] == "" {
+			options["pool"] = "1"
+		}
+		if options["namespace"] != "" {
+			namespace = options["namespace"] + ":"
+		}
+		if seconds, err := strconv.Atoi(options["poll_interval"]); err == nil {
+			pollInterval = seconds
+		} else {
+			pollInterval = 15
+		}
 
-	poolSize, _ = strconv.Atoi(options["pool"])
-	processID := options["process"]
+		poolSize, _ = strconv.Atoi(options["pool"])
+		processID := options["process"]
 
-	Config = &config{
-		processID,
-		namespace,
-		pollInterval,
-		&redis.Pool{
-			MaxIdle:     poolSize,
-			IdleTimeout: 240 * time.Second,
-			Dial: func() (redis.Conn, error) {
-				c, err := redis.Dial("tcp", options["server"])
-				if err != nil {
-					return nil, err
-				}
-				if options["password"] != "" {
-					if _, err := c.Do("AUTH", options["password"]); err != nil {
-						c.Close()
+		Config = &config{
+			processID,
+			namespace,
+			pollInterval,
+			&redis.Pool{
+				MaxIdle:     poolSize,
+				IdleTimeout: 240 * time.Second,
+				Dial: func() (redis.Conn, error) {
+					c, err := redis.Dial("tcp", options["server"])
+					if err != nil {
 						return nil, err
 					}
-				}
-				if options["database"] != "" {
-					if _, err := c.Do("SELECT", options["database"]); err != nil {
-						c.Close()
-						return nil, err
+					if options["password"] != "" {
+						if _, err := c.Do("AUTH", options["password"]); err != nil {
+							c.Close()
+							return nil, err
+						}
 					}
-				}
-				return c, err
+					if options["database"] != "" {
+						if _, err := c.Do("SELECT", options["database"]); err != nil {
+							c.Close()
+							return nil, err
+						}
+					}
+					return c, err
+				},
+				TestOnBorrow: func(c redis.Conn, t time.Time) error {
+					_, err := c.Do("PING")
+					return err
+				},
 			},
-			TestOnBorrow: func(c redis.Conn, t time.Time) error {
-				_, err := c.Do("PING")
-				return err
+			func(queue string) Fetcher {
+				return NewFetch(queue, processID, make(chan *Msg), make(chan bool))
 			},
-		},
-		func(queue string) Fetcher {
-			return NewFetch(queue, processID, make(chan *Msg), make(chan bool))
-		},
-	}
+		}
+	})
 }
